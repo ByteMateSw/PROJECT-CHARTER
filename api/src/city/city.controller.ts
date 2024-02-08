fe5e558f04bfac5f1fb613fd0dabd6a56a1e9e10
Miguel Angel Neumann <miguelangelneumann@gmail.com>
2024-02-07 21:38:35 -0300

    creado CRUD city

--- api/src/city/city.controller.ts
+++ api/src/city/city.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Body,
+  ParseIntPipe,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CityService } from './city.service';
 
 @Controller('city')
-export class CityController {}
+export class CityController {
+  constructor(private cityService: CityService) {}
+
+  @Post('/save')
+  createProvinces(@Body() newProvince) {
+    return this.cityService.createCity(newProvince);
+  }
+
+  @Get('/list')
+  getProvinces() {
+    return this.cityService.getCity();
+  }
+  @Get('/one/:id')
+  getOneProvince(@Param('id', ParseIntPipe) id: number) {
+    return this.cityService.getOneCity(id);
+  }
+
+  /*@Post('/listUpload')
+  uploadJsonLocalizations() {
+    return this.provinceService.uploadJsonLocalizations();
+  }*/
+
+  @Put('/update/:id')
+  updateProvince(
+    @Param('id', ParseIntPipe) id: number,
+    @Body()
+    province,
+  ) {
+    return this.cityService.updateCity(id, province);
+  }
+
+  @Delete('/delete/:id')
+  deleteProvince(@Param('id', ParseIntPipe) id: number) {
+    return this.cityService.deleteCity(id);
+  }
+}
--- api/src/city/city.service.ts
+++ api/src/city/city.service.ts
@@ -1,4 +1,99 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { City } from './city.entity';
 
 @Injectable()
-export class CityService {}
+export class CityService {
+  constructor(
+    @InjectRepository(City)
+    private cityRepository: Repository<City>,
+  ) {}
+
+  async createCity(newCityData) {
+    try {
+      const { name } = newCityData;
+      console.log(name);
+      const cityExist = await this.cityRepository.findOne({
+        where: { name },
+      });
+
+      if (!cityExist) {
+        const newCity = this.cityRepository.create(newCityData);
+
+        this.cityRepository.save(newCity);
+        return 'ciudad Guardada';
+      } else {
+        return 'La ciudad ya existe'; //Cuando no se manda nada tambien entra aca :V
+      }
+    } catch (error) {
+      return 'No se pudo crear la ciudad';
+    }
+  }
+
+  getCity(): Promise<City[]> | string {
+    try {
+      return this.cityRepository.find();
+    } catch (error) {
+      return 'Error al obtener las localizaciones';
+    }
+  }
+
+  async getOneCity(id: number): Promise<City | string> {
+    try {
+      if (id) {
+        const city = await this.cityRepository.findOne({
+          where: { id },
+        });
+        if (!city) {
+          return 'No se encontró la ciudad';
+        }
+        return city;
+      } else {
+        return 'Debe proporcionar un ID o un nombre de ciudad';
+      }
+    } catch (error) {
+      return 'Error al obtener la ciudad';
+    }
+  }
+
+  async updateCity(id: number, cityData): Promise<string> {
+    try {
+      if (id) {
+        const cityExist = await this.cityRepository.findOne({
+          where: { id },
+        });
+        if (!cityExist) {
+          return 'No se encontro la ciudad';
+        }
+        await this.cityRepository.update(id, cityData);
+        return 'La ciudad actualizada';
+      } else {
+        return ' No se mando la id de la ciudad ';
+      }
+    } catch (error) {
+      return 'error al actualizar ciudad';
+    }
+  }
+
+  async deleteCity(id: number): Promise<string> {
+    try {
+      if (id) {
+        const cityExist = await this.cityRepository.findOne({
+          where: { id },
+        });
+
+        if (!cityExist) {
+          return 'No se encontro la ciudad';
+        }
+        await this.cityRepository.delete({ id });
+      } else {
+        return 'Debe proporcionar un ID  de ciudad';
+      }
+
+      return 'Localizacion eliminada correctamente';
+    } catch (error) {
+      return 'Error al eliminar la localizacion';
+    }
+  }
+}
