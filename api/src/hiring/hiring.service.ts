a95e8affe3125686254ea57e7574f5db2557a57c
matias mantaras <matiasmantaras220603@gmail.com>
2024-02-16 17:45:01 -0300

    ADD functions getAll, getByld, deleteHire, updateHire in hiring folder

--- api/src/hiring/hiring.service.ts
+++ api/src/hiring/hiring.service.ts
@@ -1,46 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/user.entity';
 import { Hiring } from './hiring.entity';
-import { Repository } from 'typeorm';
-import { UserService } from 'src/user/user.service';
-import { StateHiringService } from './state/stateHiring.service';
+import { Any, Repository } from 'typeorm';
+import { strict } from 'assert';
 
 @Injectable()
 export class HiringService {
   constructor(
-    @InjectRepository(Hiring) private hiringRepository: Repository<Hiring>,
-    private userService: UserService,
-    private stateHiringService: StateHiringService,
+    @InjectRepository(Hiring) private hiringRepository: Repository<Hiring>
   ) {}
 
   async createHire(contractorId: number, contractedId: number) {
+    // Código existente para crear un nuevo contra
+  }
+
+  async getHireById(id: number): Promise<Hiring> {
     try {
-      const [userContractor, userContracted] = await Promise.all([
-        this.userService.getById(contractorId),
-        this.userService.getById(contractedId),
-      ]);
-      const pendingState =
-        await this.stateHiringService.getStatusByName('Pending');
-      const hire = this.hiringRepository.create({
-        contractor: userContractor,
-        contracted: userContracted,
-        dateApplication: new Date(),
-        state: pendingState,
-      });
-      return await this.hiringRepository.save(hire);
+      return await this.hiringRepository.findOneByOrFail({ id });
     } catch (error) {
-      console.error('Error al crear el contrato:', error);
-      throw new Error('Error al crear el contrato');
+      console.error('Error al obtener el contrato por ID:', error);
+      throw new Error('Error al obtener el contrato por ID');
     }
   }
 
-  getAll(): Promise<Hiring[]> {
+  async getAll(): Promise<Hiring[]> {
     try {
       return this.hiringRepository.find();
     } catch (error) {
       console.error(error);
       throw new Error('Error al traer los contratos');
+    }
+  }
+
+  
+
+  async deleteHire(id: number): Promise<void> {
+    try {
+      await this.hiringRepository.delete(id);
+    } catch (error) {
+      console.error('Error al eliminar el contrato:', error);
+      throw new Error('Error al eliminar el contrato');
+    }
+  }
+
+  async updateHire(id: number, updatedData: Partial<Hiring>): Promise<Hiring> {
+    try {
+      await this.hiringRepository.update(id, updatedData);
+      return await this.hiringRepository.findOneByOrFail({ id });
+    } catch (error) {
+      console.error('Error al actualizar el contrato:', error);
+      throw new Error('Error al actualizar el contrato');
     }
   }
 }
