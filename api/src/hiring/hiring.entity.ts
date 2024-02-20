11f3a195f525e5febc6282b538d1f7e96ca78076
Joha <surf234@outlook.es>
2024-02-20 10:32:53 -0300

    ADD history entity, MODIFIED hiring entity and service, MODIFIED config of db

--- api/src/hiring/hiring.entity.ts
+++ api/src/hiring/hiring.entity.ts
@@ -4,9 +4,12 @@ import {
   Column,
   ManyToOne,
   JoinColumn,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 import { User } from '../user/user.entity';
 import { StateHiring } from './state/stateHiring.entity';
+import { HistoryDate } from './history/historyDate.entity';
 
 @Entity()
 export class Hiring {
@@ -16,11 +19,12 @@ export class Hiring {
   @Column({ type: 'date' })
   dateApplication: Date;
 
-  @ManyToOne(() => StateHiring, state => state.hiring)
+  @ManyToOne(() => StateHiring, (state) => state.hiring)
   state: StateHiring;
 
-  @Column({ type: 'date', nullable: true })
-  dateAcceptOrReject: Date;
+  @ManyToMany(() => HistoryDate)
+  @JoinTable()
+  historyDate: HistoryDate[];
 
   @ManyToOne(() => User, { eager: true })
   @JoinColumn({ name: 'contractorId' })
