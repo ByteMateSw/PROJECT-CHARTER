7017459e3216b43fd78c69b2700d23e1d3ec8823
Matias Vera <matiasezequielvera@gmail.com>
2024-02-21 21:54:12 -0300

    Removed CORS and added decorator to app validator.

--- api/src/config/validations/app.validation.ts
+++ api/src/config/validations/app.validation.ts
@@ -1,4 +1,10 @@
-import { IsEnum, IsNumber, IsPositive } from 'class-validator';
+import {
+  IsDefined,
+  IsEnum,
+  IsNumber,
+  IsPositive,
+  IsUrl,
+} from 'class-validator';
 import { IEnvironment } from '../interfaces/app.interface';
 import {
   createEnumErrorMessageFormatter,
@@ -6,11 +12,21 @@ import {
 } from 'src/utils/validation/validate-util';
 
 export class AppEnvironmentVariables {
+  @IsDefined({
+    message: createErrorMessageFormatter(
+      'Se requiere el entorno en el que se ejecuta la aplicación.',
+    ),
+  })
   @IsEnum(IEnvironment, {
     message: createEnumErrorMessageFormatter(),
   })
   NODE_ENV: IEnvironment;
 
+  @IsDefined({
+    message: createErrorMessageFormatter(
+      'Se requiere el puerto de la aplicación.',
+    ),
+  })
   @IsNumber(
     { allowInfinity: false, allowNaN: false, maxDecimalPlaces: 0 },
     { message: createErrorMessageFormatter('Debe ser un número.') },
