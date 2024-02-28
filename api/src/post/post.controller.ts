286bd3cdefdf237a2e3b1c89633a88a46d62dba8
Matias Vera <matiasezequielvera@gmail.com>
2024-02-28 08:41:11 -0300

    Added return values to post controller.

--- api/src/post/post.controller.ts
+++ api/src/post/post.controller.ts
@@ -23,10 +23,11 @@ import { AccessTokenGuard } from '../auth/jwt/access.guard';
 import { UserParamID } from '../utils/params/user.param';
 import { CustomParseIntPipe } from '../utils/pipes/parse-int.pipe';
 import { FilePipeValidator } from '../utils/pipes/file-validator.pipe';
-import { File } from '../utils/types/functions.type';
+import { File, ResponseMessage } from '../utils/types/functions.type';
 import { InfoParam } from '../utils/params/info.param';
-import { EmptyBodyPipe } from 'src/utils/pipes/empty-body.pipe';
-import { QueryNumberPipe } from 'src/utils/pipes/query-number.pipe';
+import { EmptyBodyPipe } from '../utils/pipes/empty-body.pipe';
+import { QueryNumberPipe } from '../utils/pipes/query-number.pipe';
+import { ImagePost } from '../image/imagePost.entity';
 
 @Controller('posts')
 export class PostController {
@@ -48,7 +49,9 @@ export class PostController {
 
   @UseGuards(AccessTokenGuard)
   @Get(':id')
-  async getPostById(@Param(':id', ParseIntPipe) id: number) {
+  async getPostById(
+    @Param(':id', ParseIntPipe) id: number,
+  ): Promise<PostEntity> {
     return await this.postService.getPostBy({ id });
   }
 
@@ -60,7 +63,7 @@ export class PostController {
     @InfoParam() createPostDto: CreatePostDto,
     @UserParamID(CustomParseIntPipe) userId: number,
     @UploadedFiles(FilePipeValidator) images: File[],
-  ) {
+  ): Promise<PostEntity> {
     const newPost = await this.postService.createPost(userId, createPostDto);
     await this.postService.addImagesToPost(newPost.id, images);
     return newPost;
@@ -72,14 +75,17 @@ export class PostController {
   async addImageToPost(
     @Param('id', CustomParseIntPipe) postId: number,
     @UploadedFiles(FilePipeValidator) images: File[],
-  ) {
+  ): Promise<ImagePost[]> {
     return await this.postService.addImagesToPost(postId, images);
   }
 
   @UseGuards(AccessTokenGuard)
   @Delete('images/:id')
-  async removeImageFromPost(@Param('id', CustomParseIntPipe) imageId: number) {
-    return await this.postService.removeImageFromPost(imageId);
+  async removeImageFromPost(
+    @Param('id', CustomParseIntPipe) imageId: number,
+  ): Promise<ResponseMessage> {
+    await this.postService.removeImageFromPost(imageId);
+    return { message: 'La imágen se ha borrado correctamente' };
   }
 
   @UseGuards(AccessTokenGuard)
@@ -88,15 +94,17 @@ export class PostController {
   async uptadePost(
     @Param('id') id: number,
     @Body(EmptyBodyPipe) UptadePostDto: UptadePostDto,
-  ) {
+  ): Promise<ResponseMessage> {
     await this.postService.uptadePost(id, UptadePostDto);
-    return 'La publicación se actualizó correctamente';
+    return { message: 'La publicación se actualizó correctamente' };
   }
 
   @UseGuards(AccessTokenGuard)
   @Delete(':id')
-  async deletePost(@Param('id', CustomParseIntPipe) postId: number) {
+  async deletePost(
+    @Param('id', CustomParseIntPipe) postId: number,
+  ): Promise<ResponseMessage> {
     await this.postService.deletePost(postId);
-    return 'La publicación se ha borrado correctamente';
+    return { message: 'La publicación se ha borrado correctamente' };
   }
 }
