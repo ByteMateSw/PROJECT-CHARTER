3b0b806a0768766203121d2049487b88953ce1c5
Matias Vera <matiasezequielvera@gmail.com>
2024-03-01 09:22:56 -0300

    Added documentation to role folder.

--- api/src/role/role.decorator.ts
+++ api/src/role/role.decorator.ts
@@ -4,8 +4,16 @@ import { applyDecorators } from '@nestjs/common';
 import { AccessTokenGuard } from '../auth/jwt/access.guard';
 import { RoleGuard } from './role.guard';
 
+/**
+ * The key used to define roles for authorization.
+ */
 export const ROLES_KEY = 'roles';
 
+/**
+ * Decorator function that sets the metadata for the allowed roles and applies guards to the endpoint.
+ * @param roles The roles allowed to access the endpoint.
+ * @returns A decorator function that sets the metadata and applies guards.
+ */
 export const Roles = (...roles: Role[]) => {
   return applyDecorators(
     SetMetadata(ROLES_KEY, roles),
