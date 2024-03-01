f885ce3f6d4e21d7c5ba85f05a1fdd3686da1730
Matias Vera <matiasezequielvera@gmail.com>
2024-03-01 08:54:23 -0300

    Added user documentation.

--- api/src/user/dto/updateUser.dto.ts
+++ api/src/user/dto/updateUser.dto.ts
@@ -9,23 +9,38 @@ import {
   IsAlpha,
 } from 'class-validator';
 
+/**
+ * Data transfer object for updating a user.
+ */
 export class UpdateUserDto {
+  /**
+   * The first name of the user.
+   */
   @IsOptional()
   @IsString({ message: 'El nombre debe ser una cadena de caracteres.' })
   @IsNotEmpty({ message: 'El nombre no puede estar vacío.' })
   @IsAlpha('es-ES', { message: 'El nombre debe contener solo letras.' })
   firstName?: string;
 
+  /**
+   * The last name of the user.
+   */
   @IsOptional()
   @IsString({ message: 'El apellido debe ser una cadena de caracteres.' })
   @IsNotEmpty({ message: 'El apellido no puede estar vacío.' })
   @IsAlpha('es-ES', { message: 'El apellido debe contener solo letras.' })
   lastName?: string;
 
+  /**
+   * The email address of the user.
+   */
   @IsOptional()
   @IsEmail({}, { message: 'Debe ser un email válido' })
   email?: string;
 
+  /**
+   * The password of the user.
+   */
   @IsOptional()
   @IsString({ message: 'La contraseña debe ser una cadena de caracteres.' })
   @IsNotEmpty({ message: 'La contraseña no puede estar vacía.' })
@@ -44,12 +59,18 @@ export class UpdateUserDto {
   )
   password?: string;
 
+  /**
+   * The phone number of the user.
+   */
   @IsOptional()
   @IsPhoneNumber(undefined, {
     message: 'El número de teléfono debe de tener un formato válido',
   })
   numberPhone?: string;
 
+  /**
+   * The birthday of the user.
+   */
   @IsOptional()
   @IsDateString(
     { strictSeparator: false },
--- api/src/user/user.entity.ts
+++ api/src/user/user.entity.ts
@@ -14,66 +14,129 @@ import { Role } from '../role/role.entity';
 import { City } from '../city/city.entity';
 import { Notifications } from '../notifications/notifications.entity';
 
+/**
+ * Represents a User entity.
+ */
 @Entity()
 export class User {
+  /**
+   * The unique identifier of the user.
+   */
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  /**
+   * The first name of the user.
+   */
   @Column()
   firstName: string;
 
+  /**
+   * The last name of the user.
+   */
   @Column()
   lastName: string;
 
+  /**
+   * The email address of the user.
+   */
   @Column({ unique: true })
   email: string;
 
+  /**
+   * The password of the user.
+   */
   @Column({ select: false })
   password: string;
 
+  /**
+   * Indicates whether the user's account is validated.
+   */
   @Column({ default: false, select: false })
   isAccountValidate: boolean;
 
+  /**
+   * Indicates whether the user's DNI is validated.
+   */
   @Column({ default: false, select: false })
   dniValidate: boolean;
 
+  /**
+   * Indicates whether the user is deleted.
+   */
   @Column({ default: false, select: false })
   isDeleted: boolean;
 
+  /**
+   * The phone number of the user.
+   */
   @Column()
   numberPhone: string;
 
+  /**
+   * The birthday of the user.
+   */
   @Column({ type: 'date' })
   birthday: Date;
 
+  /**
+   * Indicates whether the user has accepted the Terms of Service.
+   */
   @Column({ default: false, select: false })
   acceptedToS: boolean;
 
+  /**
+   * The DNI (National Identity Document) of the user.
+   */
   @Column()
   dni: string;
 
+  /**
+   * The refresh token of the user.
+   */
   @Column({ select: false, nullable: true })
   refreshToken: string;
 
+  /**
+   * The photo of the user.
+   */
   @Column({ type: 'bytea', nullable: true })
   photo: Buffer;
 
+  /**
+   * The city associated with the user.
+   */
   @ManyToOne(() => City, city => city.users)
   city: City;
 
+  /**
+   * The offices associated with the user.
+   */
   @ManyToMany(() => Office)
   @JoinTable()
   offices: Office[];
 
+  /**
+   * The reviews made by the user.
+   */
   @OneToMany(() => Review, review => review.user)
   reviews: Review[];
 
+  /**
+   * The posts made by the user.
+   */
   @OneToMany(() => Post, post => post.user)
   posts: Post[];
 
+  /**
+   * The role of the user.
+   */
   @ManyToOne(() => Role, role => role.user)
   role: Role;
 
+  /**
+   * The notifications received by the user.
+   */
   @OneToMany(() => Notifications, notifications => notifications.user)
   notifications: Notification[];
 }
