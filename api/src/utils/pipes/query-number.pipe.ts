7716b9b0694a32dc476430a1d80e8f18e9b29911
Matias Vera <matiasezequielvera@gmail.com>
2024-03-04 12:49:08 -0300

    Modified actions to wrong values in query pipe.

--- api/src/utils/pipes/query-number.pipe.ts
+++ api/src/utils/pipes/query-number.pipe.ts
@@ -1,4 +1,4 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
 /**
  * A pipe that transforms a query parameter into a number.
@@ -7,13 +7,18 @@ import { Injectable, PipeTransform } from '@nestjs/common';
 export class QueryNumberPipe implements PipeTransform<any> {
   /**
    * Transforms the input value into a number.
-   * If the input value is not a number, it is converted to 0.
    * @param value - The value to be transformed.
+   * @param metadata - Additional information about the value.
+   * The data property contains the name of the query parameter.
+   * @throws BadRequestException if the value is not a number.
    * @returns The transformed value.
    */
-  async transform(value: any) {
-    console.log('Valor', value);
-    if (isNaN(value)) value = 0;
+  async transform(value: any, metadata: any) {
+    console.log('Metadata:', metadata);
+    if (isNaN(value))
+      throw new BadRequestException(
+        `Invalid number in ${metadata.data} query parameter`,
+      );
     return value;
   }
 }
