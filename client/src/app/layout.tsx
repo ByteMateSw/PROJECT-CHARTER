3439ba006476ca4eefd7061f4756d40099ab4cd9
lucibz <luciiabz.465@gmail.com>
2024-03-07 12:47:18 -0300

    ADD office form

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -21,7 +21,9 @@ export default function RootLayout({
       <body className={nunito.className}>
         <Header />
         {children}
+       
         <Footer />
+       
       </body>
     </html>
   );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import OfficeForm from "./components/office-form";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    <OfficeForm />
+      
     </main>
   );
 }
