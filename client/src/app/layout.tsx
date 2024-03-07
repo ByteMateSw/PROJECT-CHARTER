397ea138b3e5b077b217140212468c481f73d2a9
IsaAlegre <154946617+IsaAlegre@users.noreply.github.com>
2024-03-07 17:56:20 -0300

    ADD city folder

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
