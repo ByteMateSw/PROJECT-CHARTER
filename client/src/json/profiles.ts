78494538b38ae7f41a3eda834961e10af82013c8
Joha <surf234@outlook.es>
2024-03-11 08:28:00 -0300

    refactor: full client side refactor

---
+++ client/src/json/profiles.ts
@@ -0,0 +1,32 @@
+export const profiles = [
+    {
+      "name": "Dune",
+      "profesion": "Programador",
+      "imageProfile": "https://randomuser.me/api/portraits/men/75.jpg"
+    },
+    {
+      "name": "Foundation",
+      "profesion": "Diseñador Gráfico",
+      "imageProfile": "https://randomuser.me/api/portraits/women/76.jpg"
+    },
+    {
+      "name": "1984",
+      "profesion": "Albañil",
+      "imageProfile": "https://randomuser.me/api/portraits/men/78.jpg"
+    },
+    {
+      "name": "Brave New World",
+      "profesion": "Plomero",
+      "imageProfile": "https://randomuser.me/api/portraits/men/22.jpg"
+    },
+    {
+      "name": "The Hitchhiker's Guide to the Galaxy",
+      "profesion": "Profesor",
+      "imageProfile": "https://randomuser.me/api/portraits/women/45.jpg"
+    },
+    {
+      "name": "Neuromancer",
+      "profesion": "Cocinero",
+      "imageProfile": "https://randomuser.me/api/portraits/women/17.jpg"
+    }
+  ]
