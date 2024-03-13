223991a9b8d21a861fd7f5effdead16fe543c757
lucibz <luciiabz.465@gmail.com>
2024-03-13 09:11:49 -0300

    ADD page with styles for contract profiles and JSON with sample profile models

--- client/src/app/dashboard/hire/page.tsx
+++ client/src/app/dashboard/hire/page.tsx
@@ -1,26 +1,44 @@
-import { trabajadores } from "@/json/trabajadores";
+import { Profiles } from "@/json/hireProfiles";
 
 export default function HirePage() {
-    return(
-        <>
-        <main className="min-h-screen">
-            <section className="flex">
-                {trabajadores.map((trabajador)=>{
-                    return(
-                        <div key={trabajador.name}>
-                            <img
-                            src={trabajador.imageTrabajador}
-                            alt={trabajador.name}
-                            className="h-[220] w-[220px] rounded-full bg-secondary-gray" 
-                            />
-                            <h2>{trabajador.name}</h2>
-                            <p>{trabajador.profesion}</p>
-                            <p>{trabajador.ubicacion}</p>
-                        </div>
-                    );
-                })}
-                </section>
-        </main>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <section className="flex flex-wrap justify-center p-10">
+          {Profiles.map((profile) => {
+            return (
+              <div
+                className="p-5 bg-slate-50 rounded-2xl border border-neutral-400 m-4"
+                key={profile.name}
+              >
+                <img
+                  src={profile.imageProfile}
+                  alt={profile.name}
+                  className="h-[240px] w-[260px] rounded-full bg-secondary-gray"
+                />
+                <div className="flex-col items-center flex justify-between mt-4">
+                  <h2 className="text-center text-secondary-black text-3xl font-bold">
+                    {profile.name}
+                  </h2>
+                  <p className="text-center text-secondary-gray text-base font-normal">
+                    {profile.profesion}
+                  </p>
+                </div>
+                <div className="flex justify-between items-end w-full mt-4">
+                  <p className="text-secondary-black text-xs font-bold text-start">
+                    {profile.ubicacion}
+                  </p>
+                  <button className="w-[104px] h-[38px] px-4 bg-blue-600 rounded-lg">
+                    <p className="text-secondary-white text-base font-bold ">
+                      contratar
+                    </p>
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </main>
+    </>
+  );
 }
---
+++ client/src/json/hireProfiles.ts
@@ -0,0 +1,20 @@
+export const Profiles = [
+  {
+    imageProfile: "https://randomuser.me/api/portraits/men/75.jpg",
+    name: "John Doe",
+    profesion: "Programador",
+    ubicacion: "Buenos Aires, Argentina",
+  },
+  {
+    imageProfile: "https://randomuser.me/api/portraits/men/75.jpg",
+    name: "Maria Lopez",
+    profesion: "Electricista",
+    ubicacion: "Santa Fe, Argentina",
+  },
+  {
+    imageProfile: "https://randomuser.me/api/portraits/men/75.jpg",
+    name: "Laura Gomez",
+    profesion: "Plomero",
+    ubicacion: "Chubut, Argentina",
+  },
+];
