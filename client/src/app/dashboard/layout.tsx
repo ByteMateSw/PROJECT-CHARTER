a7669fb66688326282f5529ed90a0667eb14ac64
Joha <surf234@outlook.es>
2024-03-25 10:18:26 -0300

    style: the sidebar has been modified

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Header />
     </div>
       <div className="flex">
-        <div className="w-80 fixed h-full overflow-auto  mt-14">
+        <div className="w-80 fixed h-[89.5%] mt-20 ml-4">
           <Sidebar />
         </div>
         <div className="flex-grow ml-80 mt-6">
