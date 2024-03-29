4b4891cc87d1ed168ebe86abfdaab991ac646003
matias mantaras <matiasmantaras220603@gmail.com>
2024-03-29 09:08:56 -0300

    refactor:  start the changes to the modification of work cards

--- client/src/app/dashboard/jobs/page.tsx
+++ client/src/app/dashboard/jobs/page.tsx
@@ -1,28 +1,23 @@
-import { cardDetails } from "@/json/cards"
+import { cardDetails } from "@/json/cards";
 
 export default function JobsPage() {
   return (
     <div className="h-screen w-full flex justify-center flex-wrap">
       {cardDetails.map((cards) => {
-        return(
-          <div className="bg-se shadow-md p-6 rounded-xl w-30 h-13 flex-shrink-0"
-          key={cards.title}
-          >
-           
-          <h2 className="text-2xl font-bold mb-2">
-            {cards.title}
-          </h2>
-          <h3 className="text-xl italic mb-4">Subtitle</h3>
-          <p className="text-gray-700"> texxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxt</p>
-
-      </div>
-        
-        
+        return (
+          <div className="bg-se shadow-md p-6 rounded-xl w-30 h-13 flex-shrink-0" style={{ width: '29.125rem', height: '0.66631rem', flexShrink: 0 }} key={cards.title}>
+            <h2 className="text-2xl font-bold mb-2">
+              {cards.title}<div className="text">necesito gente en servicio de cañerias</div>
+            </h2>
+            <div className="vertical-text">
+              <h3 className="text-xl italic mb-4">necesito de alguien que me arregle unas cañerias .</h3>
+              <p className="text-gray-700"> 
+                .<br/>  .<br/>
+              </p>
+            </div>
+          </div>
         );
       })}
-
-
-      
-</div>
-  )
-    }
+    </div>
+  );
+}
