d44e7a599f2a4068ca352bf1968cff92679258d8
franquito15k <gabrielfrangomez146@gmail.com>
2024-03-29 10:35:15 -0300

    Feat: added api to the sidebar

--- client/src/app/dashboard/Sidebar.tsx
+++ client/src/app/dashboard/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { use, useState, useEffect } from "react";
 import { professions } from "@/json/professions";
 import Image from "next/image";
 
@@ -9,15 +9,31 @@ export default function Sidebar(): JSX.Element {
   );
   const measure = 24;
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [users, setUsers] = useState<User[]>([]);
+  interface User {
+    id: number;
+    name: string;
+  }
+  const URL = "http://localhost:3001/offices";
 
-  const handleCheckboxChange = (index: number) => {
+  const showData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    showData();
+  }, []);
+
+  const handleCheckboxChange = (id: number) => {
     setCheckedItems((prevCheckedItems) => {
       const updatedCheckedItems = { ...prevCheckedItems };
 
-      if (updatedCheckedItems[index]) {
-        delete updatedCheckedItems[index];
+      if (updatedCheckedItems[id]) {
+        delete updatedCheckedItems[id];
       } else {
-        updatedCheckedItems[index] = true;
+        updatedCheckedItems[id] = true;
       }
 
       return updatedCheckedItems;
@@ -48,25 +64,29 @@ export default function Sidebar(): JSX.Element {
         </section>
 
         <ul className="overflow-y-scroll w-full mt-6">
-          {professions
-            .filter((profesion) =>
-              profesion.name.toLowerCase().includes(searchTerm.toLowerCase())
+          {users
+            .filter((users) =>
+              users.name.toLowerCase().includes(searchTerm.toLowerCase())
             )
-            .map((profesion, index) => {
+            .map((user, id) => {
               return (
-                <li key={index} className="flex items-center py-1 w-fit" onClick={()=> handleCheckboxChange(index)}>
+                <li
+                  key={id}
+                  className="flex items-center py-1 w-fit"
+                  onClick={() => handleCheckboxChange(id)}
+                >
                   <input
                     className={`ml-2 rounded-full appearance-none w-2 h-2 ring-2 ring-offset-2 ring-secondary-black items-center justify-center ${
-                      checkedItems[index]
+                      checkedItems[id]
                         ? " bg-primary-blue ring-2"
                         : "bg-secondary-white"
                     }`}
-                    id={`${index}`}
+                    id={`${id}`}
                     type="checkbox"
-                    checked={checkedItems[index] || false}
+                    checked={checkedItems[id] || false}
                   />
                   <label className="text-neutral-900 text-base ml-2">
-                    {profesion.name}
+                    {user.name}
                   </label>
                 </li>
               );
@@ -75,26 +95,28 @@ export default function Sidebar(): JSX.Element {
         <hr className="w-full mt-6" />
         <section className="w-full flex flex-col flex-1 justify-end items-start py-6 gap-4">
           <div className="flex">
-          <Image
-            src="/svg/location.svg"
-            alt="Location Icon"
-            width={measure}
-            height={measure}
-          />
-          <span className="ml-1 text-secondary-black text-xl font-bold">Ubicación</span>
+            <Image
+              src="/svg/location.svg"
+              alt="Location Icon"
+              width={measure}
+              height={measure}
+            />
+            <span className="ml-1 text-secondary-black text-xl font-bold">
+              Ubicación
+            </span>
           </div>
           <select className="flex justify-between items-center self-stretch  bg-secondary-white text-secondary-gray border p-2 border-secondary-gray rounded-2xl appearance-none">
             <option value="default">Provincia</option>
-            {professions.map((profesion, index) => (
-              <option key={index} value={profesion.name_country}>
+            {professions.map((profesion, id) => (
+              <option key={id} value={profesion.name_country}>
                 {profesion.name_country}
               </option>
             ))}
           </select>
           <select className="flex justify-between items-center self-stretch  bg-secondary-white text-secondary-gray border p-2 border-secondary-gray rounded-2xl appearance-none">
             <option value="default">Ciudad</option>
-            {professions.map((profesion, index) => (
-              <option key={index} value={profesion.name}>
+            {professions.map((profesion, id) => (
+              <option key={id} value={profesion.name}>
                 {profesion.name}
               </option>
             ))}
