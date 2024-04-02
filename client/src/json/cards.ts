6814e94e8faa2917725e504ed4e2cec9b8473737
IsaAlegre <154946617+IsaAlegre@users.noreply.github.com>
2024-04-02 09:59:25 -0300

    Build: new dependencies have been added

--- client/src/json/cards.ts
+++ client/src/json/cards.ts
@@ -1,25 +1,25 @@
 export const cardDetails = [
   {
-    imgUrl: "https://picsum.photos/id/237/200/300",
+    imgUrl: "https://humanidades.com/wp-content/uploads/2017/10/trabajo-1-e1571886459117.jpg",
     title: "Text 1",
   },
   {
-    imgUrl: "https://picsum.photos/id/238/200/300",
+    imgUrl: "https://humanidades.com/wp-content/uploads/2017/10/trabajo-1-e1571886459117.jpg",
     title: "Text 2",
   },
 
   {
-    imgUrl: "https://picsum.photos/id/239/200/300",
+    imgUrl: "https://media.diariolasamericas.com/p/76068694b8cb8b803f5da8d289241eea/adjuntos/216/imagenes/100/088/0100088907/855x0/smart/dia-mundial-la-costurera-pexelsjpg.jpg",
     title: "Text 3",
   },
 
   {
-    imgUrl: "https://picsum.photos/id/240/200/300",
+    imgUrl: "https://img.freepik.com/foto-gratis/profesor-ayudando-nina-tarea_329181-14267.jpg",
     title: "Text 4",
   },
 
   {
-    imgUrl: "https://picsum.photos/id/241/200/300",
+    imgUrl: "https://img.freepik.com/foto-gratis/sirva-tecnico-electrico-que-trabaja-centralita-fusibles_169016-24062.jpg",
     title: "Text 5",
   },
 
