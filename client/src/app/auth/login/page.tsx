5529081354f39e93d28502f4c6338f7c5a2f22e0
IsaAlegre <154946617+IsaAlegre@users.noreply.github.com>
2024-04-03 10:17:59 -0300

    Add logic for google login from front-end

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -1,13 +1,46 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { gapi } from "gapi-script";
+import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from "react-google-login";
+
 
 export default function LoginPage() {
+  const GOOGLE_CLIENT_ID = '483719238317-0b67hs4cfkkhbr17ieikrknd9h7oib12.apps.googleusercontent.com'
+  
+  const [user, setUser] = useState({}); 
+  
+
+  if (typeof window !== 'undefined') {
+     useEffect(() => {
+      const start = () => {
+        gapi.auth2.init({
+          clientId: GOOGLE_CLIENT_ID, 
+        })
+      }
+      gapi.load("client:auth2", start)
+    }, [])
+  }
+
+  const onSuccess = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    if ('profileObj' in response) {
+      console.log(response.profileObj);
+    }else{
+      console.error('profileObj is not present in the response');
+    }
+  }
+  const onFailure = (response: any) => {
+    console.log("error")
+  }
+
+  
   return (
-    <section className="min-h-screen flex justify-around items-center bg-secondary-white">
+    <section className="min-h-screen flex justify-around items-center bg-secondary-white overflow-hidden">
       <picture className="flex justify-around ">
         <img src="/svg/Imagotype.svg" alt="Logotype" />
       </picture>
 
-      <article className="w-[550px] px-12 py-12 bg-secondary-white flex flex-col">
+      <article className="w-[550px] px-12 py-8 bg-secondary-white flex flex-col">
         <div className="flex justify-center items-center flex-col">
           <img
             className="mb-6 "
@@ -74,7 +107,7 @@ export default function LoginPage() {
           </button>
         </div>
 
-        <div className="w-full flex flex-col justify-center mt-8">
+        <div className="w-full flex flex-col justify-center my-4 overflow-hidden">
           <button
             id="submit"
             type="submit"
@@ -82,6 +115,15 @@ export default function LoginPage() {
           >
             Continuar
           </button>
+        
+          <GoogleLogin
+            clientId={GOOGLE_CLIENT_ID}
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            buttonText="Continue with Google"
+            className="btn w-full md:h-full  bg-secondary-white rounded-xl flex justify-center my-4 hover:scale-105"
+          />
+          
           <p className="flex justify-center">
             Sos nuevo?{" "}
             <Link href="" className="text-primary-blue hover:underline ml-1">
@@ -94,3 +136,4 @@ export default function LoginPage() {
     </section>
   );
 }
+
