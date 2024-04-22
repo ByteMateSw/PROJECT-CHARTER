5fcf7aa307b7bb946f6661fb6c7b2aa11b2540eb
Joha <surf234@outlook.es>
2024-04-21 22:42:31 -0300

    feat: more changes

--- client/src/app/api/user/index.ts
+++ client/src/app/api/user/index.ts
@@ -3,14 +3,29 @@ import { User } from "../interface";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-
 export const getBestUsers = async (): Promise<User[]> => {
-    try {
-        const response = await axios.get(`https://political-jenn-bytemate.koyeb.app/user/best-users`);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
+  try {
+    const response = await axios.get(
+      `https://political-jenn-bytemate.koyeb.app/user/best-users`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const login = async (email: string, password: string): Promise<User> => {
+  try {
+    const response = await axios.post(
+      `https://political-jenn-bytemate.koyeb.app/auth/login`,
+      { email, password },
+      { withCredentials: false }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
