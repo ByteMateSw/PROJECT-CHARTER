3980a7d9ecee57031965438c0ea123ef55d0950a
johaortiz <surf234@outlook.es>
2024-05-19 21:51:42 -0300

    fixed style card hire and modal

--- client/src/app/components/Dashboard/HireModal.tsx
+++ client/src/app/components/Dashboard/HireModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import React from "react";
 
 export default function HireModal({
   user,
@@ -8,74 +9,93 @@ export default function HireModal({
   user: any;
   index: number;
 }) {
-  // Mejorar el diseño de este modal, principalmente el código HTML
   return (
     <>
       <label className="w-full btn btn-primary rounded-full" htmlFor={`modal-${index}`}>
         Ver Perfil
       </label>
       <input className="modal-state" id={`modal-${index}`} type="checkbox" />
-      <div className="modal">
+      <section className="modal">
         <label className="modal-overlay" htmlFor={`modal-${index}`}></label>
-        <div className="modal-content flex flex-col gap-5 p-0">
-          <div className="flex flex-col text-center w-full">
+        <article className="modal-content h-full w-full flex flex-col gap-5 p-0 rounded-[2rem]">
+          {/* Foto de perfíl */}
+          <div className="flex flex-col w-full justify-center items-center">
             <img
               src={user.imageBackground}
               alt="Imagen de fondo"
-              className="w-full h-40 object-cover rounded-t-2xl"
+              className="w-full h-36 object-cover rounded-t-[2rem]"
             />
-            <div className="flex justify-center items-center flex-col -mt-20 space-x-4">
-              <img
-                src={user.imageProfile}
-                alt="imagen de perfil"
-                className="h-32 w-32 rounded-full bg-secondary-gray border-2 border-secondary-white select-none"
-              />
-            </div>
-            <section className="px-6 mx-auto max-w-[500px]">
-              <h3 className="text-bold text-xl font-black text-secondary-black">
-                {user.name}
-              </h3>
-              <p className="text-sm text-secondary-gray w-full">
-                {user.profession}
-              </p>
-              <p className="text-left">
-                <strong>Ubicación:</strong> {user.location}
-              </p>
-              <p className="text-left">
-                <strong>Descripción:</strong> {user.description}
-              </p>
-              <div className="flex justify-center pt-4 gap-4">
-                <Link href={`/profile/${user.id}`}>
-                  <button className="btn px-6 bg-primary-green text-secondary-white font-bold">
-                    Ir al perfil completo
-                  </button>
-                </Link>
-              </div>
-            </section>
-            <hr className="border-t border-secondary-gray mt-8" />
-            <section>
-              <div className="ml-8 flex flex-col flex-1 my-3">
-                <label className="flex flex-start flex-1">Lorem ipsum</label>
-                <ul className="flex gap-2">
-                  <li className="h-10 w-10 rounded-lg bg-secondary-gray">.</li>
-                  <li className="h-10 w-10 rounded-lg bg-secondary-gray">.</li>
-                  <li className="h-10 w-10 rounded-lg bg-secondary-gray">.</li>
-                  <li className="h-10 w-10 rounded-lg bg-secondary-gray">.</li>
-                </ul>
-              </div>
-            </section>
-            <hr className="border-t border-secondary-gray" />
-            <section className="pb-4">
-              <p className="text-start ml-8 my-5">Lorem ipsum</p>
-              <div className="flex justify-center">
-                <div className="flex gap-3">
-                  <label htmlFor={`modal-${index}`} className="btn bg-primary-green text-secondary-white">Cancel</label>
-                </div>
-              </div>
-            </section>
+            <img
+              src={user.imageProfile}
+              alt="imagen de perfil"
+              className="h-36 w-36 rounded-full flex justify-center items-center flex-col -mt-20 space-x-4 bg-secondary-gray border-2 border-secondary-white select-none"
+            />
           </div>
-        </div>
-      </div>
+          {/* Nombre, Ubicación, Trabajos y Puntuación */}
+          <div className="px-4 pb-2">
+            <h2 className="text-secondary-blackt text-center text-3xl font-bold">{user.name}</h2>
+            <span className="flex justify-center items-center">
+              <img
+                src="/svg/Location-Icon.svg"
+                alt="image"
+                className="inline h-5 w-5 mr-1"
+              />
+              <p className="text-secondary-black text-xs font-bold">{user.location}</p>
+            </span>
+            <p className="text-secondary-gray text-center text-base font-normal my-2">
+              {user.professions.map((profession: any, index: number) => (
+                <React.Fragment key={index}>
+                  {profession}
+                  {index !== user.professions.length - 1 && ", "}
+                </React.Fragment>
+              ))}
+            </p>
+            <span className="w-full inline-flex items-center justify-center">
+              {
+                [0, 1, 2, 3, 4].map((item, index) => {
+                  return (
+                    <img
+                      src="/svg/star.svg"
+                      alt="image"
+                      className="inline h-5 w-5"
+                    />
+                  )
+                })
+              }
+              <p className="ml-2 text-secondary-black text-xs font-bold">{user.review}</p>
+            </span>
+          </div>
+          <div className="pt-4 px-9 text-justify">
+            <h2 className="text-secondary-black text-2xl font-bold">Sobre Mí</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cupiditate modi deserunt dolor laudantium perspiciatis? Qui reiciendis at omnis sed architecto quia nihil, consequatur possimus dolorum ut minima! Optio, ex.</p>
+          </div>
+          <div className="pt-2 px-9 text-justify">
+            <h2 className="text-secondary-black text-2xl font-bold">Servicios</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cupiditate modi deserunt dolor laudantium perspiciatis? Qui reiciendis at omnis sed architecto quia nihil, consequatur possimus dolorum ut minima! Optio, ex.</p>
+          </div>
+          <ul className="inline-flex py-4 justify-center items-center gap-x-10">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+          </ul>
+          <div className="flex justify-center items-center gap-x-6">
+            <label htmlFor={`modal-${index}`} className="btn bg-primary-blue text-secondary-white">Cerrar</label>
+            <Link href={`/profile/${user.name}`}>
+              <label htmlFor={`modal-${index}`} className="btn bg-primary-blue text-secondary-white">Ver Perfil Completo</label>
+            </Link>
+          </div>
+        </article>
+      </section>
     </>
   );
 }
+
+
+/*
+
+<div className="flex gap-3">
+                  <label htmlFor={`modal-${index}`} className="btn bg-primary-green text-secondary-white">Cancel</label>
+                </div>
+*/
