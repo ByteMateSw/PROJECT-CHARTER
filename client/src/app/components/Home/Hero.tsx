0baee02f1260d0b18102d073a1f8996b84a059f3
ApsTris <surf234@outlook.es>
2025-02-05 16:59:17 -0300

    quita de botones del hero del home

--- client/src/app/components/Home/Hero.tsx
+++ client/src/app/components/Home/Hero.tsx
@@ -10,14 +10,14 @@ export default function Hero() {
             <span>Encontrar</span> talento.<br />
           </h1>
         </div>
-        <div className="hidden md:flex gap-14 pt-4 h-14 font-bold text-xl [&>button]:flex [&>button]:items-center [&>button]:px-6 [&>button]:py-4 [&>button]:bg-primary-green [&>button]:rounded-full">
+        {/* <div className="hidden md:flex gap-14 pt-4 h-14 font-bold text-xl [&>button]:flex [&>button]:items-center [&>button]:px-6 [&>button]:py-4 [&>button]:bg-primary-green [&>button]:rounded-full">
           <button className="hover:scale-110 duration-150">
             Encontrar Talento!
           </button>
           <button className="hover:scale-110 duration-150">
             Busca Trabajo!
           </button>
-        </div>
+        </div> */}
       </article>
       <article className="hidden xl:flex justify-end h-full items-end w-2/4">
         <img
