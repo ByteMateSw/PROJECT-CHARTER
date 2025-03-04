bf2ada0c540cf822135ad03a3736d759738fd434
Pedro Fernandez <pefernandez1408@gmail.com>
2025-03-04 11:27:10 -0400

    se cambio el formato del banner

--- client/src/app/AdsenseBanner.tsx
+++ client/src/app/AdsenseBanner.tsx
@@ -21,13 +21,22 @@ const AdBanner = () => {
 
   const banner = useRef<HTMLDivElement>(null)
 
+  const atOptions = {
+		'key' : 'c2966c28682c8c93dd8577b3912cdff3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+
   useEffect(() => {
     if (banner.current && !banner.current.firstChild) {
     const conf = document.createElement('script')
     const script = document.createElement('script')
     script.type = 'text/javascript'
-    script.src = "//pl26016389.effectiveratecpm.com/488d3100fddbe57811e8fa6e0875087a/invoke.js"
-    
+    script.src = "//www.highperformanceformat.com/c2966c28682c8c93dd8577b3912cdff3/invoke.js"
+    conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+
     banner.current.append(conf)
     banner.current.append(script)
     }
@@ -43,7 +52,7 @@ const AdBanner = () => {
     //   data-full-width-responsive={dataFullWidthResponsive.toString()}
     // ></ins>
     <div id="container-488d3100fddbe57811e8fa6e0875087a"
-      className="mt-4"
+      className="flex justify-center mt-28 w-full"
       ref={banner}
     ></div>
   );
