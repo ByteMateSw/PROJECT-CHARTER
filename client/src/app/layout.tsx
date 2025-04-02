67583dce2cec9e11666e314cbf0172a22f30f957
johaortiz <surf234@outlook.es>
2025-04-02 16:04:21 -0300

    Falta de pago

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 import Footer from "./components/Footer";
 import SessionAuthProvider from "@/context/SessionAuthProvider";
 import { UserProvider } from "@/context/userContext";
-import { SearchProvider } from "@/context/searchContext"
+import { SearchProvider } from "@/context/searchContext";
 import GoogleAdsense from "./GoogleAdsense";
 
 export default function RootLayout({
@@ -14,31 +14,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const noHeader = ["/auth"];
-  const noFooter = ["auth", "dashboard", "profile", "settings"];
-
-  const hideHeader = noHeader.some((route) => pathname.includes(route));
-  const hideFooter = noFooter.some((route) => pathname.includes(route));
-
   return (
     <html lang="es" className="bg-secondary-white text-secondary-black">
       <head>
         {/* <GoogleAdsense pId={process.env.NEXT_PUBLIC_GOOGLEADS as string} /> */}
-        <meta name="google-adsense-account" content="ca-pub-2868851941806861"></meta>
+        {/* <meta name="google-adsense-account" content="ca-pub-2868851941806861"></meta> */}
         {/* <script async data-cfasync="false" src="//pl26016389.effectiveratecpm.com/488d3100fddbe57811e8fa6e0875087a/invoke.js"></script> */}
       </head>
-      <SessionAuthProvider>
-        <UserProvider>
-          <SearchProvider>
-          <body className={`${nunito.className} antialiased h-screen w-full`}>
-            {hideHeader ? null : <Header />}
-            {children}
-            {hideFooter ? null : <Footer />}
-          </body>
-          </SearchProvider>
-        </UserProvider>
-      </SessionAuthProvider>
+      <body className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl md:text-5xl font-bold text-center text-red-600">
+          El servicio de Conectando ha sido deshabilitado temporalmente <br />{" "}
+          hasta recibir lo acordado.
+        </h1>
+      </body>
     </html>
   );
 }
